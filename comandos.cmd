f8d2d3f7c51354bdfd8c96d4df125d3c67ca6c5a
rickgds80 <38327991+rickgds80@users.noreply.github.com>
2020-10-07 19:18:11 -0300

    Add files via upload

--- comandos.cmd
+++ comandos.cmd
@@ -1,48 +1,47 @@
-#zookeeper start
-C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows>zookeeper-server-start.bat C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\config\zookeeper.properties
+rem #zookeeper start
+cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
+zookeeper-server-start.bat C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\config\zookeeper.properties
 
+
+rem zookeeper-server-start.bat C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\config\zookeeper.properties & KAFKA_OPTS="$KAFKA_OPTS -javaagent:C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows\jmx_prometheus_javaagent-0.2.0.jar=7071:C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows\kafka-0-8-2.yml" 
+
+
+rem #kafka start
+cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
+kafka-server-start.bat ..\..\config\server.properties
+
+
+rem #criar um topico
 cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
 
-#kafka start
-C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows>kafka-server-start.bat ..\..\config\server.properties
+rem #criar um topico
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic TESTE
 
-
-
-cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
-
-#criar um topico
-kafka-topics.bat --create --bootstrap-server localhost:2181 --replication-factor 1 --partitions 1 --topic ECOMMERCE_NEW_ORDER
-kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ECOMMERCE_NEW_ORDER
-
-#listar topicos
+rem #listar topicos
 kafka-topics.bat --list --zookeeper localhost:2181
-###kafka-topics.bat --list --bootstrap-server localhost:2181
 
-
-#describe dos topicos
-kafka-topics.bat --bootstrap-server localhost:9092 --describe
-
-
-#deletar
-kafka-topics.bat --zookeeper localhost:2181 --delete --topic ECOMMERCE_NEW_ORDER
+rem #describe dos topicos
+kafka-topics.bat --bootstrap-server 127.0.0.1:9092 --describe
 
 
 
+rem #produtor
+cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
+kafka-console-producer.bat --broker-list localhost:9092 --topic TESTE
 
-#produtor
-kafka-console-producer.bat --broker-list localhost:9092 --topic teste
+rem #consumidor
+cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
+kafka-console-consumer.bat --topic TESTE --bootstrap-server localhost:9092
+kafka-console-consumer.bat --topic TESTE --from-beginning --bootstrap-server localhost:9092
 
-#consumidor
-kafka-console-consumer.bat --topic teste --from-beginning --bootstrap-server localhost:9092
 
-#consultar os grupos entrada e o já consumidor
+ rem # mostrar desenho da aplicacao
+ rem #abrir Java
+ 
+
+
+rem #consultar os grupos entrada e o consumidor
+cd C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\bin\windows
 kafka-consumer-groups.bat --bootstrap-server 127.0.0.1:9092 --list --command-config C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\config\consumer.properties
 kafka-consumer-groups.bat --bootstrap-server 127.0.0.1:9092 --all-groups --command-config C:\kafka_2.13-2.6.0\kafka_2.13-2.6.0\config\consumer.properties --describe
 
- 
- 
-#splunk start
-C:\Program Files\Splunk\bin
-splunk.exe start
-splunk.exe status
-splunk.exe stop
